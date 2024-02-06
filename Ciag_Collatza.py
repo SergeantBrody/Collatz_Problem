a362e0fe066d8beafed7074d025733b5885eb0ba
Filip Andrzejczyk <filipandrzejczyk123@gmail.com>
2024-02-06 22:01:31 +0100

    utworzenie pliku

---
+++ Ciag_Collatza.py
@@ -0,0 +1,11 @@
+x = int(input("Podaj liczbe od 1 do 100"))
+if x < 1 or x > 100:
+    print("Liczba musi być z przedzialu 1 do 100 !")
+else:
+    while x != 1:
+        print(x)
+        if x % 2 == 0:
+            x = x / 2
+        else:
+            x = 3 * x + 1
+print(x)
