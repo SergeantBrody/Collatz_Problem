72213712d05460bb770e90f501701f111d9ad87a
Filip Andrzejczyk <filipandrzejczyk123@gmail.com>
2024-02-06 22:26:03 +0100

    poprawiono ponownie

--- Ciag_Collatza.py
+++ Ciag_Collatza.py
@@ -1,8 +1,6 @@
 x = int(input("Podaj liczbe od 1 do 100"))
 if x < 1 or x > 100:
     print("Liczba musi być z przedzialu 1 do 100 !")
-elif x == 1:
-    print(x)
 else:
     while x != 1:
         print(x)
@@ -10,4 +8,4 @@ else:
             x = x / 2
         else:
             x = 3 * x + 1
-
+    print(x)
